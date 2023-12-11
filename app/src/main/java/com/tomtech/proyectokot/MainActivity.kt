3c87b636a467d5c667333ff22a43b86092a38985
kinepax <kinepax@hotmail.com>
2023-12-11 09:40:22 -0500

    Aprendizaje ciclo de vida de Activity

---
+++ app/src/main/java/com/tomtech/proyectokot/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.tomtech.proyectokot
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatTextView
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Encuentra la AppCompatTextView con el id txtName
+        val eName = findViewById<AppCompatTextView>(R.id.txtName)
+
+        // Ahora puedes utilizar eName como referencia a tu TextView
+        // Por ejemplo, para establecer un texto
+        eName.text = "Hola, mundo!"
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume();
+        Log.d("MainActivity","onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity","onPause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity","onStop")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity","onDestroy")
+    }
+}
